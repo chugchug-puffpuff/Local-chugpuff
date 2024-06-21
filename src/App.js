d1e1b7d306ced5f0569e2158e19b0a8205acc9a2
Enen13 <eunho9813@gmail.com>
2024-06-21 11:43:29 +0900

    feat: 메인페이지 임시 제작

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainPage from './MainPage/MainPage.js';
+
+function App() {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainPage/MainPage.js
@@ -0,0 +1,396 @@
+import React from "react";
+import "./MainPage.css";
+
+export const MainPage = () => {
+  return (
+    <div className="screen">
+      <div className="frame">
+        <div className="text-wrapper">실시간 인기글</div>
+        <div className="div">더보기</div>
+      </div>
+      <div className="frame-2">
+        <div className="frame-3">
+          <div className="frame-4">
+            <div className="div-wrapper">
+              <div className="text-wrapper-2">정보 공유</div>
+            </div>
+            <div className="frame-5">
+              <div className="frame-6">
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Sms"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/sms@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Favorite"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/favorite@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+              </div>
+              <div className="text-wrapper-4">2024.05.07</div>
+            </div>
+          </div>
+          <p className="text-wrapper-5">이직 결정 전 고려해야 할 사항들 - 현직자 경험담 공유</p>
+        </div>
+        <div className="frame-3">
+          <div className="frame-4">
+            <div className="frame-8">
+              <div className="text-wrapper-6">취업 고민</div>
+            </div>
+            <div className="frame-5">
+              <div className="frame-6">
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Sms"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/sms@2x.png"
+                  />
+                  <div className="text-wrapper-7">15</div>
+                </div>
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Favorite"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/favorite@2x.png"
+                  />
+                  <div className="text-wrapper-7">15</div>
+                </div>
+              </div>
+              <div className="text-wrapper-8">2024.05.07</div>
+            </div>
+          </div>
+          <div className="text-wrapper-5">공정관리와 생산관리 직무 차이</div>
+        </div>
+        <div className="frame-3">
+          <div className="frame-4">
+            <div className="frame-8">
+              <div className="text-wrapper-6">취업 고민</div>
+            </div>
+            <div className="frame-5">
+              <div className="frame-6">
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Sms"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/sms@2x.png"
+                  />
+                  <div className="text-wrapper-7">15</div>
+                </div>
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Favorite"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/favorite@2x.png"
+                  />
+                  <div className="text-wrapper-7">15</div>
+                </div>
+              </div>
+              <div className="text-wrapper-8">2024.05.07</div>
+            </div>
+          </div>
+          <p className="text-wrapper-5">휴학 사유 솔직하게 말해도 될까요?</p>
+        </div>
+        <div className="frame-3">
+          <div className="frame-4">
+            <div className="div-wrapper">
+              <div className="text-wrapper-2">정보 공유</div>
+            </div>
+            <div className="frame-5">
+              <div className="frame-6">
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Sms"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/sms@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Favorite"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/favorite@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+              </div>
+              <div className="text-wrapper-4">2024.05.07</div>
+            </div>
+          </div>
+          <p className="text-wrapper-5">전자공 전공인데 배터리 반도체는 연관성 없는 사업인가요?</p>
+        </div>
+        <div className="frame-3">
+          <div className="frame-4">
+            <div className="frame-8">
+              <div className="text-wrapper-2">취업 고민</div>
+            </div>
+            <div className="frame-5">
+              <div className="frame-6">
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Sms"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/sms@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Favorite"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/favorite@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+              </div>
+              <div className="text-wrapper-4">2024.05.07</div>
+            </div>
+          </div>
+          <p className="text-wrapper-5">전자공 전공인데 배터리 반도체는 연관성 없는 사업인가요?</p>
+        </div>
+        <div className="frame-3">
+          <div className="frame-4">
+            <div className="frame-8">
+              <div className="text-wrapper-2">취업 고민</div>
+            </div>
+            <div className="frame-5">
+              <div className="frame-6">
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Sms"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/sms@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Favorite"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/favorite@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+              </div>
+              <div className="text-wrapper-4">2024.05.07</div>
+            </div>
+          </div>
+          <p className="text-wrapper-5">전자공 전공인데 배터리 반도체는 연관성 없는 사업인가요?</p>
+        </div>
+        <div className="frame-3">
+          <div className="frame-4">
+            <div className="frame-8">
+              <div className="text-wrapper-2">취업 고민</div>
+            </div>
+            <div className="frame-5">
+              <div className="frame-6">
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Sms"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/sms@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+                <div className="frame-7">
+                  <img
+                    className="img"
+                    alt="Favorite"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/favorite@2x.png"
+                  />
+                  <div className="text-wrapper-3">15</div>
+                </div>
+              </div>
+              <div className="text-wrapper-4">2024.05.07</div>
+            </div>
+          </div>
+          <p className="text-wrapper-5">전자공 전공인데 배터리 반도체는 연관성 없는 사업인가요?</p>
+        </div>
+      </div>
+      <div className="frame-9">
+        <div className="text-wrapper">실시간 인기 공고</div>
+        <div className="text-wrapper-9">더보기</div>
+      </div>
+      <div className="view">
+        <div className="overlap-group">
+          <img
+            className="image"
+            alt="Image"
+            src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/6672cc7b5445d7af1e4bee20/img/---.png"
+          />
+          <div className="view-2">
+            <div className="view-3">
+              <div className="frame-10">
+                <p className="p">취업 준비, 이제 혼자가 아니다.</p>
+              </div>
+              <div className="frame-10">
+                <p className="text-wrapper-10">
+                  완성형 조력자
+                  <br />
+                  치치폭폭과 함께 취업 뽀개기!
+                </p>
+              </div>
+            </div>
+            <div className="view-4">
+              <div className="text-wrapper-11">AI 모의면접 시작하기</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="overlap">
+        <img
+          className="image-2"
+          alt="Image"
+          src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/6672cc7b5445d7af1e4bee20/img/image-1@2x.png"
+        />
+        <div className="frame-11">
+          <div className="text-wrapper-12">(주)그레이고</div>
+          <div className="frame-12">
+            <div className="frame-13">
+              <p className="text-wrapper-13">패션 마케터 과/차장급 (5-12년) 경력직</p>
+            </div>
+            <div className="frame-14">
+              <div className="frame-15">
+                <div className="grade-wrapper">
+                  <img
+                    className="grade"
+                    alt="Grade"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/grade@2x.png"
+                  />
+                </div>
+                <div className="frame-16">
+                  <div className="text-wrapper-14">50</div>
+                </div>
+              </div>
+              <div className="text-wrapper-15">D-12</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="overlap-2">
+        <img
+          className="image-2"
+          alt="Image"
+          src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/6672cc7b5445d7af1e4bee20/img/image-1@2x.png"
+        />
+        <div className="frame-11">
+          <div className="text-wrapper-12">(주)그레이고</div>
+          <div className="frame-12">
+            <div className="frame-13">
+              <p className="text-wrapper-13">패션 마케터 과/차장급 (5-12년) 경력직</p>
+            </div>
+            <div className="frame-14">
+              <div className="frame-15">
+                <div className="grade-wrapper">
+                  <img
+                    className="grade"
+                    alt="Grade"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/grade@2x.png"
+                  />
+                </div>
+                <div className="frame-16">
+                  <div className="text-wrapper-14">50</div>
+                </div>
+              </div>
+              <div className="text-wrapper-15">D-12</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="overlap-3">
+        <img
+          className="image-2"
+          alt="Image"
+          src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/6672cc7b5445d7af1e4bee20/img/image-1@2x.png"
+        />
+        <div className="frame-11">
+          <div className="text-wrapper-12">(주)그레이고</div>
+          <div className="frame-12">
+            <div className="frame-13">
+              <p className="text-wrapper-13">패션 마케터 과/차장급 (5-12년) 경력직</p>
+            </div>
+            <div className="frame-14">
+              <div className="frame-15">
+                <div className="grade-wrapper">
+                  <img
+                    className="grade"
+                    alt="Grade"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/grade@2x.png"
+                  />
+                </div>
+                <div className="frame-16">
+                  <div className="text-wrapper-14">50</div>
+                </div>
+              </div>
+              <div className="text-wrapper-15">D-12</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="overlap-4">
+        <img
+          className="image-3"
+          alt="Image"
+          src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/6672cc7b5445d7af1e4bee20/img/image-2@2x.png"
+        />
+        <div className="frame-11">
+          <div className="text-wrapper-12">(주)그레이고</div>
+          <div className="frame-12">
+            <div className="frame-13">
+              <p className="text-wrapper-13">패션 마케터 과/차장급 (5-12년) 경력직</p>
+            </div>
+            <div className="frame-14">
+              <div className="frame-15">
+                <div className="grade-wrapper">
+                  <img
+                    className="grade"
+                    alt="Grade"
+                    src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/666f93a3d0304f0ceff1aa35/img/grade@2x.png"
+                  />
+                </div>
+                <div className="frame-16">
+                  <div className="text-wrapper-14">50</div>
+                </div>
+              </div>
+              <div className="text-wrapper-15">D-12</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="frame-wrapper">
+        <div className="frame-17">
+          <div className="text-wrapper-16">
+            회사소개&nbsp;&nbsp;|&nbsp;&nbsp;인재채용&nbsp;&nbsp;|&nbsp;&nbsp;제휴제안&nbsp;&nbsp;|&nbsp;&nbsp;이용약관&nbsp;&nbsp;|&nbsp;&nbsp;개인정보처리방침
+          </div>
+          <div className="text-wrapper-17">© 2024 AppFlight</div>
+        </div>
+      </div>
+      <div className="view-wrapper">
+        <div className="view-5">
+          <div className="element">
+            <div className="text-wrapper-18">로그인</div>
+            <div className="text-wrapper-18">회원가입</div>
+          </div>
+          <div className="navbar">
+            <div className="text-wrapper-18">AI 모의면접</div>
+            <div className="text-wrapper-18">자기소개서 첨삭</div>
+            <div className="text-wrapper-18">취업공고</div>
+            <div className="text-wrapper-18">커뮤니티</div>
+            <div className="text-wrapper-18">캘린더</div>
+          </div>
+          <div className="view-6">
+            <div className="view-7" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
