1db99fcd96603dd453deeab61980b69611c4fd6a
Enen13 <eunho9813@gmail.com>
2024-07-04 19:23:56 +0900

    fix: 메인페이지 인기글-불러온 데이터에 따라 값이 변하도도록 수정

--- src/MainPage/MainComponent/RTPosts.js
+++ src/MainPage/MainComponent/RTPosts.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './RTPosts.css';
 import PopularPost from './RTPComponents/PopularPost';
+import postData from '../../TestData/postData.json';
 
 const RTPosts = () => {
+  const [sortedPostData, setSortedPostData] = useState([]);
+
+  useEffect(() => {
+    const sortedData = postData.sort((a, b) => b.favorites - a.favorites).slice(0, 7);
+    setSortedPostData(sortedData);
+  }, []);
+
   return (
     <div>
       <div className="RTPosts-frame">
@@ -10,55 +18,16 @@ const RTPosts = () => {
         <div className="RTPosts-div">더보기</div>
       </div>
       <div className="RTPosts-frame-2">
-        <PopularPost
-          category="정보 공유"
-          date="2024.05.07"
-          title="이직 결정 전 고려해야 할 사항들 - 현직자 경험담 공유"
-          comments="15"
-          favorites="15"
-        />
-        <PopularPost
-          category="취업 고민"
-          date="2024.05.07"
-          title="공정관리와 생산관리 직무 차이"
-          comments="15"
-          favorites="15"
-        />
-        <PopularPost
-          category="취업 고민"
-          date="2024.05.07"
-          title="휴학 사유 솔직하게 말해도 될까요?"
-          comments="15"
-          favorites="15"
-        />
-        <PopularPost
-          category="정보 공유"
-          date="2024.05.07"
-          title="전자공 전공인데 배터리 반도체는 연관성 없는 사업인가요?"
-          comments="15"
-          favorites="15"
-        />
-        <PopularPost
-          category="취업 고민"
-          date="2024.05.07"
-          title="전자공 전공인데 배터리 반도체는 연관성 없는 사업인가요?"
-          comments="15"
-          favorites="15"
-        />
-        <PopularPost
-          category="취업 고민"
-          date="2024.05.07"
-          title="전자공 전공인데 배터리 반도체는 연관성 없는 사업인가요?"
-          comments="15"
-          favorites="15"
-        />
-        <PopularPost
-          category="취업 고민"
-          date="2024.05.07"
-          title="전자공 전공인데 배터리 반도체는 연관성 없는 사업인가요?"
-          comments="15"
-          favorites="15"
-        />
+        {sortedPostData.map((post, index) => (
+          <PopularPost
+            key={index}
+            category={post.category}
+            date={post.date}
+            title={post.title}
+            comments={post.comments}
+            favorites={post.favorites}
+          />
+        ))}
       </div>
     </div>
   )
