8515744eea6e1f3cd1892868409a53d6d7adaab9
Enen13 <eunho9813@gmail.com>
2024-08-04 23:59:24 +0900

    fix: 폴더구조 재 변경

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+const jwt = require('../src/Backend/node_modules/jsonwebtoken');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('../src/Backend/node_modules/cors/lib');
 
 const app = express();
 const port = 4000;
