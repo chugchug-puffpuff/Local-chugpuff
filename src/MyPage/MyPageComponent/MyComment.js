e190a2b16003a5e006257577ed5c99a5e19ede42
Enen13 <eunho9813@gmail.com>
2024-09-18 21:52:46 +0900

    fix: 마이페이지-내가 작성한 댓글(게시글댓글) 구현

--- src/MyPage/MyPageComponent/MyComment.js
+++ src/MyPage/MyPageComponent/MyComment.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './MyComment.css'
-import { useNavigate } from 'react-router-dom'
-// import Pagination from '../../Route/Pagination.js';
+import { useNavigate, Link } from 'react-router-dom'
+import axios from 'axios';
+import Pagination from '../../Route/Pagination.js';
 
-const Post = ({ title, category, comments, likes, date }) => (
+// 날짜 형식을 0000-00-00 00:00:00으로 변환
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const datePart = date.toISOString().split('T')[0];
+  const timePart = date.toTimeString().split(' ')[0];
+
+  return `${datePart} ${timePart}`;
+};
+
+const Post = ({ boardNo, boardTitle, category, boardDate, commentCount, likes, liked }) => (
   <div className="MyComment-view">
     <div className="MyComment-frame-9">
       <div className="MyComment-frame-10">
-        <p className="MyComment-p">{title}</p>
+        <Link to={`/communitypost/${boardNo}`}>
+          <p className="MyComment-p">{boardTitle}</p>
+        </Link>
         <div className={category === "정보공유" ? "MyComment-frame-11" : "MyComment-frame-14"}>
           <div className="MyComment-text-wrapper-5">{category}</div>
         </div>
@@ -20,18 +32,18 @@ const Post = ({ title, category, comments, likes, date }) => (
               alt="Sms"
               src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/6688fccfcda281749136af44/img/sms@2x.png"
             />
-            <div className="MyComment-text-wrapper-6">{comments}</div>
+            <div className="MyComment-text-wrapper-6">{commentCount}</div>
           </div>
           <div className="MyComment-frame-13">
             <img
-              className="MyComment-img"
+              className={`MyComment-img ${liked ? 'liked' : ''}`}
               alt="Favorite"
               src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/6688fccfcda281749136af44/img/favorite@2x.png"
             />
             <div className="MyComment-text-wrapper-6">{likes}</div>
           </div>
         </div>
-        <div className="MyComment-text-wrapper-7">{date}</div>
+        <div className="MyComment-text-wrapper-7">{formatDate(boardDate)}</div>
       </div>
     </div>
   </div>
@@ -39,65 +51,55 @@ const Post = ({ title, category, comments, likes, date }) => (
 
 const MyComment = () => {
   const [selectedComment, setSelectedComment] = useState('post');
+  const [posts, setPosts] = useState([]);
+  const [likedPosts, setLikedPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  // 사용자가 작성한 댓글이 달린 게시글 목록 가져오기
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const config = {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        };
 
-  const posts = [
-    {
-      title: '이직 결정 전 고려해야 할 사항들 - 현직자 경험담 공유',
-      category: '정보공유',
-      comments: 15,
-      likes: 15,
-      date: '2024.05.07',
-    },
-    {
-      title: '이직 결정 전 고려해야 할 사항들 - 현직자 경험담',
-      category: '취업고민',
-      comments: 20,
-      likes: 20,
-      date: '2024.05.08',
-    },
-    {
-      title: '면접 시 주의사항',
-      category: '정보공유',
-      comments: 30,
-      likes: 17,
-      date: '2024.05.10',
-    },
-    {
-      title: '모두들 취업 화이팅 하세요!',
-      category: '취업고민',
-      comments: 10,
-      likes: 17,
-      date: '2024.05.11',
-    },
-    {
-      title: '면접 시 주의사항',
-      category: '정보공유',
-      comments: 30,
-      likes: 20,
-      date: '2024.05.10',
-    },
-    {
-      title: '모두들 취업 화이팅 하세요!',
-      category: '취업고민',
-      comments: 10,
-      likes: 17,
-      date: '2024.05.11',
-    },
-    {
-      title: '면접 시 주의사항',
-      category: '정보공유',
-      comments: 30,
-      likes: 17,
-      date: '2024.05.10',
-    },
-    {
-      title: '모두들 취업 화이팅 하세요!',
-      category: '취업고민',
-      comments: 10,
-      likes: 17,
-      date: '2024.05.11',
-    }
-  ]
+        const commentResponse = await axios.get('http://localhost:8080/api/comment/user', config);
+        const boardNos = commentResponse.data.map(comment => comment.boardNo);
+
+        const boardPromises = boardNos.map(boardNo => 
+          axios.get(`http://localhost:8080/api/board/${boardNo}`, config)
+        );
+
+        const boardResponses = await Promise.all(boardPromises);
+        const mappingData = boardResponses.map(response => ({
+          boardNo: response.data.boardNo,
+          boardTitle: response.data.boardTitle,
+          category: response.data.category.categoryName,
+          boardDate: response.data.boardDate,
+          commentCount: response.data.commentCount,
+          likes: response.data.likes
+        }));
+
+        setPosts(mappingData);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    const fetchLikedPosts = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/board/liked', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        });
+        setLikedPosts(response.data.map(post => post.boardNo));
+      } catch (error) {
+        console.error('Error fetching liked posts:', error);
+      }
+    };
+    fetchComments();
+    fetchLikedPosts();
+  }, []);
 
   // 탭 이동
   const navigate = useNavigate()
@@ -108,6 +110,12 @@ const MyComment = () => {
   const handleCommentClick = (type) => {
     setSelectedComment(type);
   };
+
+  const postsPerPage = 5;
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   return (
     <div className="MyComment-frame">
@@ -131,7 +139,7 @@ const MyComment = () => {
                 className={`${selectedComment === 'post' ? 'selected-comments' : 'MyComment-text-wrapper-4'}`}
                 onClick={() => handleCommentClick('post')}
               >
-                게시글 댓글 (2건)
+                게시글 댓글 ({posts.length}건)
               </div>
               <div
                 className={`${selectedComment === 'job' ? 'selected-comments' : 'MyComment-text-wrapper-4'}`}
@@ -142,10 +150,10 @@ const MyComment = () => {
             </div>
             <div className="MyComment-frame-7">
               <div className="MyComment-frame-8">
-                {posts.map((post, index) => (
+                {currentPosts.map((post, index) => (
                   <React.Fragment key={index}>
-                    <Post {...post} />
-                    {index < posts.length - 1 && (
+                    <Post {...post} liked={likedPosts.includes(post.boardNo)} />
+                    {index < currentPosts.length - 1 && (
                       <img
                         className="MyComment-line"
                         alt="Line"
@@ -155,7 +163,7 @@ const MyComment = () => {
                   </React.Fragment>
                 ))}
               </div>
-              {/* <Pagination totalPages={totalPages} currentPage={currentPage} setCurrentPage={setCurrentPage} scrollTop={0} /> */}
+              <Pagination totalPages={totalPages} currentPage={currentPage} setCurrentPage={setCurrentPage} scrollTop={0} />
             </div>
           </div>
         </div>
