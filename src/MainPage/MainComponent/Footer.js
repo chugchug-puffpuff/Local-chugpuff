0b0b63ae95fd9d48af10544a7d8a2eeea0ee9351
Enen13 <eunho9813@gmail.com>
2024-10-14 23:21:11 +0900

    fix: 메인페이지-디자인팀 이름 추가

--- src/MainPage/MainComponent/Footer.js
+++ src/MainPage/MainComponent/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="Footer-frame-wrapper">
         <div className="Footer-frame">
           <div className="Footer-text-wrapper">
-            Capstone Design&nbsp;&nbsp;|&nbsp;&nbsp;소프트웨어&서비스컴퓨팅전공&nbsp;&nbsp;|&nbsp;&nbsp;김도연 신은호 윤예지
+            Capstone Design&nbsp;&nbsp;|&nbsp;&nbsp;소프트웨어&서비스컴퓨팅전공&nbsp;&nbsp;|&nbsp;&nbsp;김도연 신은호 윤예지&nbsp;&nbsp;|&nbsp;&nbsp;Design By 백기석 장은솔
           </div>
           <div className="Footer-text-wrapper-2">© 2024 AppFlight</div>
         </div>
