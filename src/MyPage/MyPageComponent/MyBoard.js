3e0a5713aec4e77e091b4d7c41a671fde8450e3f
Enen13 <eunho9813@gmail.com>
2024-11-08 20:50:57 +0900

    라인 이미지 수정

--- src/MyPage/MyPageComponent/MyBoard.js
+++ src/MyPage/MyPageComponent/MyBoard.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import Pagination from '../../Route/Pagination.js';
 import sms_icon from '../../Icon/sms.png'
 import favorite_icon from '../../Icon/favorite.png'
+import line_4_icon from '../../Icon/Line_4.png'
 
 // 날짜 형식을 0000-00-00 00:00:00으로 변환
 const formatDate = (dateString) => {
@@ -136,7 +137,7 @@ const MyBoard = () => {
                       <img
                         className="MyBoard-line"
                         alt="Line"
-                        src="https://cdn.animaapp.com/projects/666f9293d0304f0ceff1aa2f/releases/6688fccfcda281749136af44/img/line-4.svg"
+                        src={line_4_icon}
                       />
                     )}
                   </React.Fragment>
